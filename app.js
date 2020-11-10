f35e3710e76d41ff4f66c2b707f9b19130e07044
Evyn Newton <newton.evyn@gmail.com>
2020-11-10 15:26:10 -0500

    starting on some psuedocode

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('Hello World')
+
+let upcomingLaunchesURL = 'https://ll.thespacedevs.com/2.0.0/launch/upcoming/ '
+let searchQuery = ' '
+let searchUpcoming = `https://ll.thespacedevs.com/2.0.0/launch/upcoming?search=${searchQuery}`
+
+  //Get input from search bar
+  //Search data pull function
+  //Create and display counters
+
+  //save counters into local storage
