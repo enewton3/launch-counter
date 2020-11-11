6b1c6008a1e5e86c4238b8b663b9ed704628867e
Evyn Newton <newton.evyn@gmail.com>
2020-11-11 18:43:14 -0500

    populating data into the DOM

--- app.js
+++ app.js
@@ -5,12 +5,25 @@ let searchQuery = ' '
 let searchUpcoming = `https://ll.thespacedevs.com/2.0.0/launch/upcoming?search=${searchQuery}`
 const access_token = config.access_token
 const searchBar = document.querySelector('#search-bar')
-const 
+const counterContainer = document.querySelector('.counter-container')
+
+let fiveLaunches = []
+
+class launch {
+  constructor(name, type, image, url, details, time) {
+    this.name = name
+    this.type = type
+    this.image = image
+    this.url = url
+    this.details = details
+    this.time = time
+  }
+}
 
 const headers = {
   headers: {
-    'Authorization': `token ${access_token}`
-    }
+    'Authorization': `token ${access_token}`,
+    },
   }
   //Get input from search bar
 
@@ -24,16 +37,35 @@ async function get5launches() {
       let launchType = launchData[i].mission.type
       let launchImage = launchData[i].image
       let launchURL = launchData[i].url
+      let launchTime = launchData[i].net
       let launchDetails = launchData[i].mission.description
-      console.log(launchName, launchType, launchImage, launchURL, launchDetails)
+      let thisLaunch = new launch(launchName, launchType, launchImage, launchURL, launchDetails, launchTime)
+      fiveLaunches.push(thisLaunch)
       //use these variables to create an object for each launch that can stay in local storage
     }
+    displayLaunches(fiveLaunches)
   } catch (error) {
     console.log(error)
   }
 }
 
-// get5launches()
+get5launches()
+
+function displayLaunches(arr) {
+  arr.forEach((item) => {
+    let counterDiv = document.createElement('div')
+    counterDiv.className = 'counter'
+    counterDiv.backgroundImage = `url(${item.image})`
+    counterDiv.innerHTML = `<h2 class='countdown'>COUNTER GOES HERE</h2><h3>${item.name}</h3><p class='type'>${item.type}</p><p class='details-hidden'>${item.details}</p><a href='${item.url}'>Click here for more details</a>`
+    counterContainer.append(counterDiv)
+    
+  })
+}
+
+function displayDetails(event) {
+  event.preventDefault()
+  
+}
 
 //Create and display counters
 //search display function
